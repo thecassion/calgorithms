c9b3a4316fbc68ded03ba6d5a4e6c77e6ed7ebd2
thecassion <pierrerobentz.cassion@gmail.com>
2020-07-06 13:44:40 -0400

    Insertion Sort implementation

--- src/init/sort.c
+++ src/init/sort.c
@@ -61,3 +61,16 @@ int qPartition(int arr[], int first, int last){
     arr[last]=temp;
     return i;
 }
+
+void insertionSort(int arr[], int n){
+    int i, j, key;
+    for(i=1;i<n;i++){
+        key=arr[i];
+        j=i-1;
+        while(j>-1 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=key;
+    }
+}
